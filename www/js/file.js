25642dfb00af89aae064e4618329489be1e8146c
Van Reeth <jonas.vanreeth@yrbrands.com>
2016-04-25 14:17:27 +0200

    record and send

--- www/js/file.js
+++ www/js/file.js
@@ -1,29 +1,14 @@
-function writeFile() {
+var previous = '';
+function writeFile(lat, long, time) {
+    console.log('in write');
     var type = window.TEMPORARY;
   var size = 5*1024*1024;
 
   window.requestFileSystem(type, size, successCallback, errorCallback)
 
   function successCallback(fs) {
-      console.log('succes');
+      console.log('1 succes');
 
-      var previous = '';
-
-      fs.root.getFile('log.txt', {}, function(fileEntry) {
-
-         fileEntry.file(function(file) {
-            var reader = new FileReader();
-
-            reader.onloadend = function(e) {
-              previous = this.result;
-              console.log(previous);
-            };
-
-            reader.readAsText(file);
-
-         }, errorCallback);
-
-      }, errorCallback);
 
 
      fs.root.getFile('log.txt', {create: true}, function(fileEntry) {
@@ -31,14 +16,18 @@ function writeFile() {
         fileEntry.createWriter(function(fileWriter) {
 
            fileWriter.onwriteend = function(e) {
-              alert('Write completed.');
+            //   alert('Write completed.');
            };
 
            fileWriter.onerror = function(e) {
               alert('Write failed: ' + e.toString());
            };
 
-           var blob = new Blob([previous + ' Lorem Ipsum'], {type: 'text/plain'});
+           console.log('succes');
+           var input = previous +lat+':'+long +':'+ time + '\n';
+           var blob = new Blob([input], {type: 'text/plain'});
+           previous = input;
+           console.log(previous);
            fileWriter.write(blob);
         }, errorCallback);
 
@@ -67,6 +56,7 @@ function readFile() {
             reader.onloadend = function(e) {
                var txtArea = document.getElementById('textarea');
                txtArea.value = this.result;
+               previous = this.result;
             };
 
             reader.readAsText(file);
@@ -82,23 +72,87 @@ function readFile() {
 
 }
 
-function removeFile() {
+function removeFile(lat, long, time) {
+    console.log('in remove');
+    var type = window.TEMPORARY;
+    var size = 5*1024*1024;
+
+  window.requestFileSystem(type, size, successCallback, errorCallback)
+
+  function successCallback(fs) {
     fs.root.getFile('log.txt', {create: true}, function(fileEntry) {
 
-       fileEntry.createWriter(function(fileWriter) {
+        fileEntry.createWriter(function(fileWriter) {
 
-          fileWriter.onwriteend = function(e) {
-             alert('Write completed.');
-          };
+           fileWriter.onwriteend = function(e) {
+            //   alert('Write completed.');
+           };
 
-          fileWriter.onerror = function(e) {
-             alert('Write failed: ' + e.toString());
-          };
+           fileWriter.onerror = function(e) {
+              alert('Write failed: ' + e.toString());
+           };
 
-          var blob = new Blob([''], {type: 'text/plain'});
-          fileWriter.write(blob);
+            var blob = new Blob([''], {type: 'text/plain'});
+           fileWriter.write(blob);
+        }, errorCallback);
+
+     }, errorCallback);
+
+  }
+
+  function errorCallback(error) {
+     alert("ERROR: " + error.code)
+  }
+}
+
+
+
+
+
+
+function sendFile(){
+
+    var text = '';
+    var type = window.TEMPORARY;
+    var size = 5*1024*1024;
+
+    window.requestFileSystem(type, size, successCallback, errorCallback)
+
+    function successCallback(fs) {
+
+       fs.root.getFile('log.txt', {}, function(fileEntry) {
+
+          fileEntry.file(function(file) {
+             var reader = new FileReader();
+
+             reader.onloadend = function(e) {
+                text = this.result.split('\n');
+                // console.log(text);
+                $.each(text, function( key, value ) {
+                //   console.log( key + ": " + value );
+                  var data = value.split(':');
+                  console.log(data[0]);
+                  var url = "https://mzd-jonasvr.c9users.io/tag/"+data[0]+"/"+data[1]+"/"+data[2];
+                  console.log(url);
+                  $.ajax({ //gegevens gaan ophalen
+                      type:'GET',
+                      url: url,
+                      success:function(data){
+                          console.log(data);
+                      }});
+                });
+             };
+
+             reader.readAsText(file);
+
+          }, errorCallback);
+
        }, errorCallback);
+    }
 
-    }, errorCallback);
+    function errorCallback(error) {
+       alert("ERROR: " + error.code)
+    }
+
 
 }
---
+++ www/js/location.js
@@ -0,0 +1,38 @@
+// functie word niet gebruikt => was om te testen hoe snel er werd gegaan en hoe nauwkeurig de locatie werd bepaald
+function tracker(){
+    console.log('in');
+   function onSuccess(position) {
+    var lat  = position.coords.latitude;
+        lat  = lat.toFixed(6);
+    var long = position.coords.longitude;
+        long = long.toFixed(6);
+    var date = new Date(position.timestamp);
+
+    // Hours part from the timestamp
+    var day = date.getDay();
+    // Hours part from the timestamp
+    var month = date.getMonth();
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+    var time = day + '-' + month + ' ' + hours + '-' + minutes.substr(-2) + '-' + seconds.substr(-2);
+    writeFile(lat, long, time);
+
+
+
+   }
+
+   // onError Callback receives a PositionError object
+   //
+   function onError(error) {
+       alert('code: '    + error.code    + '\n' +
+             'message: ' + error.message + '\n');
+   }
+
+   // Options: throw an error if no update is received every 30 seconds.
+   //
+   var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000,enableHighAccuracy: true, maximumAge: 10*1000 });
+}
